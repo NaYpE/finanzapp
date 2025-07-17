5167add384a1ec6f276b0dd91d376426393a7f4b
Angel Castro <angel_c_g@hotmail.com>
2025-07-16 18:27:17 -0600

    feat(frontend): Se agregaron vistas y navegacion
    - index, login y signup
    - navegacion entre ellas con sus respectivos endpoints

---
+++ cmd/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+
+	"finanzapp/routes"
+)
+
+func main() {
+
+	// Cargar .env
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("No se pudo cargar .env")
+	}
+
+	// Conexión a BD
+	//config.ConnectDatabase()
+
+	router := gin.Default()
+
+	// Servir archivos estáticos (opcional)
+	//router.Static("/static", "./static") //public en el otro
+
+	// Cargar templates
+	router.LoadHTMLGlob("templates/*.html")
+
+	// Rutas
+	routes.RegisterWebRoutes(router)
+
+	// Puerto
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8181"
+	}
+
+	log.Printf("Servidor corriendo en http://localhost:%s", port)
+	router.Run(":" + port)
+}
---
+++ routes/routes.go
@@ -0,0 +1,25 @@
+package routes
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func RegisterWebRoutes(router *gin.Engine) {
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", nil)
+	})
+
+	router.GET("/login", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "login.html", nil)
+	})
+
+	router.GET("/signup", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "signup.html", nil)
+	})
+
+	router.GET("/contact", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "contact.html", nil)
+	})
+}
